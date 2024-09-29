b2320bf24d0e224b74adbc514842d85d94dc3d5d
21Alul21 <augustine.alul@yahoo.com>
2024-09-29 06:42:04 +0100

    added code for routing

--- practice-project/src/App.js
+++ practice-project/src/App.js
@@ -1,63 +1,93 @@
 import { useState } from "react";
-
-
-
-
+import { useEffect } from "react";
+import Home from "./pages/about";
+import About from "./pages/home";
+import {Routes, Route} from 'reac-router-dom';
 
 function App() {
+  // useEffect(() => {
+  //  window.alert('you are increasing');
+  // })
 
   let [value, SetValue] = useState(0);
 
   const DecreaseValue = () => {
-   value--;
-   SetValue(value);
-  }
+    value--;
+    SetValue(value);
+  };
 
   const IncreaseValue = () => {
     value++;
     SetValue(value);
-  }
+  };
 
   const DecrementIncrementCounter = () => {
+    if (value > 20) {
+      return <p>o boy you slow down na, e don pass 20 oh.</p>;
+    }
 
     return (
-    <div style={{width: '80%', padding: '50px', backgroundColor: 'blue', color: 'white', display: 'flex'}}>
-        
-        <div style={{ border: 'solid white', width: '100px', height: '100px', backgroundColor: 'white', color: 'black', display: 'flex', 'justifyContent': "center", textAlign: "center", flexDirection: "column"}}>{ value }</div>
-        <button onClick={DecreaseValue} style={{ padding: '40px', backgroundColor: 'red', color: 'white' }}>-</button>
-        <button onClick={IncreaseValue} style={{ padding: '40px', backgroundColor: 'green', color: 'white' }}>+</button>
-    </div>
-    
-    )
-    
-    }
-    
+      <div
+        style={{
+          width: "80%",
+          padding: "50px",
+          backgroundColor: "blue",
+          color: "white",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            border: "solid white",
+            width: "100px",
+            height: "100px",
+            backgroundColor: "white",
+            color: "black",
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            flexDirection: "column",
+          }}
+        >
+          {value}
+        </div>
+        <button
+          onClick={DecreaseValue}
+          style={{ padding: "40px", backgroundColor: "red", color: "white" }}
+        >
+          -
+        </button>
+        <button
+          onClick={IncreaseValue}
+          style={{ padding: "40px", backgroundColor: "green", color: "white" }}
+        >
+          +
+        </button>
+      </div>
+    );
+  };
 
-  return(
+  return (
     <>
-    <p style={{ width: '80%', backgroundColor: "blue", padding: '20px', margin: 'auto', color: 'white' }}>INCREMENT AND DECREAMENT USING USETATE COMPONENT HOOK</p>
-    <DecrementIncrementCounter/>
+      <p
+        style={{
+          width: "80%",
+          backgroundColor: "blue",
+          padding: "20px",
+          margin: "auto",
+          color: "white",
+        }}
+      >
+        INCREMENT AND DECREAMENT USING USETATE COMPONENT HOOK
+      </p>
+      <DecrementIncrementCounter />
+      <About />
+      <Home />
     </>
-    
-  )
+  );
 }
 
-
-
-
-
-
-
-
-
 export default App;
-
-
-
-
-
-
-
 
 /*
 
@@ -116,5 +146,3 @@ const Book = (props) => {
 
 
 */
-
-
